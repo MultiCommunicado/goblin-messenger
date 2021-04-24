a288a94fa75b545feda20db03cb5fd49d42841cf
jenessachap <jenessachap@gmail.com>
2021-04-24 08:24:29 -0700

    updated signup dropdown

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -13,6 +13,12 @@ import React, { Component } from 'react';
                     <form method="POST" action='/signup'>
                         <input name="username" type="text" placeholder="username"></input>
                         <input name="password" type="password" placeholder="password"></input>
+                            //dropdown box to select user's language. -jc
+                            <select className="Select" id="usersLanguage" name="usersLanguage" onChange="do something to handle change">
+                                <options value="EN"> English </options>
+                                <options value="SP"> Spanish </options>
+                                <options value="FR"> French </options>
+                            </select>
                         <input  type="submit" value="Create User"></input>
                     </form>
                 </div>    
