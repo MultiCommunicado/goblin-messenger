f9d9f7c4804281ead6e566c9ff07e8df6f66965d
jenessachap <jenessachap@gmail.com>
2021-04-24 17:51:08 -0700

    Auth functionality complete

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -2,7 +2,9 @@ const cookieController = {};
 
 // store the user id in a cookie for auth
 cookieController.setSSIDCookie = (req, res, next) => {
+    console.log(`we're in the cookie controller now`)
     res.cookie('ssid', res.locals.user._id, { httpOnly: true});
+    console.log(`This is the response for the set cookie from the db ${res.locals} `)
     return next();
 };
 
--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -7,7 +7,6 @@ const sessionController = {};
 * verify whether or not the session is still valid.
 */
 sessionController.isLoggedIn = (req, res, next) => {
-  // write code here
   Session.findOne({ cookieID: req.cookies.ssid }, (err, session) => {
     if (err) return next(`Error in sessionController.isLoggedIn: ${JSON.strigify(err)}`);
     //no session found, send back nothing for now...
@@ -21,11 +20,15 @@ sessionController.isLoggedIn = (req, res, next) => {
 * startSession - create and save a new Session into the database.
 */
 sessionController.startSession = (req, res, next) => {
-  console.log("Session Creator")
+  console.log("now in Session Creator")
   Session.create({ cookieID: res.locals.user._id}, (err, session) => {
-    if (err) return next(`Error in sessionController.startSession: ${err}`); 
+    console.log(`this is the session reponse from the db: ${session}`)
+    if (err) {
+      console.log(err);
+      return next(`Error in sessionController.startSession: ${err}`); 
+    }
     return next();
   })
 };
-
+ 
 module.exports = sessionController;
