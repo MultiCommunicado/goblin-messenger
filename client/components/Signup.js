82dc28679989b66235167f6448aad3a220941dfa
Achilleees <achille.pe.perducat@gmail.com>
2021-04-25 22:39:11 -0400

    login/signup styling

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -4,19 +4,20 @@ import React from 'react';
 //with "signup" button and signup link
 
 const Signup = props => (
-    <div className="Login">
-        <form method="POST" action='/signup'>
-            <input name="username" type="text" placeholder="username"></input>
-            <input name="password" type="password" placeholder="password"></input>
-            <label> Select your language 
+    <div className="loginSignup">
+        <div className="loginSignupForm">
+            <form method="POST" action='/signup'>
+                <h1>Signup</h1>
+                <input name="username" type="text" placeholder="username"></input><br/>
+                <input name="password" type="password" placeholder="password"></input><br/>
                 <select className="Select" id="usersLanguage" name="usersLanguage" onChange="do something to handle change">
                     <option value="EN"> English </option>
                     <option value="SP"> Spanish </option>
                     <option value="FR"> French </option>
                 </select>
-            </label>
-            <input  type="submit" value="Create User"></input>
-        </form>
+                <input  type="submit" value="Create User"></input>
+            </form>
+        </div>
     </div>
 );
 
