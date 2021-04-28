eb213b19ecc6d7a34259d7d739feabc15fddfd32
kerri <75552647+kerriannercrawford@users.noreply.github.com>
2021-04-27 20:32:26 -0400

    Changes made to webpack config and package.json to allow for JEST functionality. .babelrc created for jest. Addition to test files.

--- __tests__/reducerTest.js
+++ __tests__/reducerTest.js
@@ -1,19 +1,58 @@
-// - Default State
+//const subject = require('../client/state/reducers/mainReducer.js');
 
+describe('Reducer Tests', () => {
+  let state;
 
-// - LOGIN_STATE
+  beforeEach(() => {
+    state = {};
+  });
 
+  // - Default State
+  describe('should return true', () => {
+    it('should return true', () => {
+      expect('true').toEqual('true');
+    });
+  });
 
-// - LOGIN
+  // - LOGIN_STATE
+  describe('', () => {
+    xit('should', () => {
+      expect();
+    });
+  });
 
+  // - LOGIN
+  describe('', () => {
+    xit('should', () => {
+      expect();
+    });
+  });
 
-// - SIGNUP_STATE
+  // - SIGNUP_STATE
+  describe('', () => {
+    xit('should', () => {
+      expect();
+    });
+  });
 
+  // - SIGNUP
+  describe('', () => {
+    xit('should', () => {
+      expect();
+    });
+  });
 
-// - SIGNUP
+  // - USER_INFO
+  describe('', () => {
+    xit('should', () => {
+      expect();
+    });
+  });
 
-
-// - USER_INFO
-
-
-// - UPDATE_MESSAGES
+  // - UPDATE_MESSAGES
+  describe('', () => {
+    xit('should', () => {
+      expect();
+    });
+  });
+});
