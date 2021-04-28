a19e4b629dd6f0b62bd74f121ed16a51d2d1b3c3
Seymour-creates <romelogilbert@yahoo.com>
2021-04-28 11:20:51 -0400

    weeeee

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -21,7 +21,7 @@ sessionController.isLoggedIn = (req, res, next) => {
 */
 sessionController.startSession = (req, res, next) => {
   console.log("now in Session Creator")
-  /* if (!res.locals.user) */ return next();
+   if (!res.locals.user)  return next();
   if (!session) { try {
       Session.update({ cookieID: res.locals.user._id }, {upsert: true}, (err, session) => {
         console.log(`this is the session reponse from the db: ${session}`)
